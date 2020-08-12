3f41690eb07a3f9c70626725084c09721622dc2f
Erik Seliger <erikseliger@me.com>
2020-08-13 01:47:44 +0200

    Cleanup campaign details for overhaul

--- web/src/enterprise/campaigns/detail/CampaignDetails.tsx
+++ web/src/enterprise/campaigns/detail/CampaignDetails.tsx
@@ -1,19 +1,18 @@
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import AlertCircleIcon from 'mdi-react/AlertCircleIcon'
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { HeroPage } from '../../../components/HeroPage'
 import { PageTitle } from '../../../components/PageTitle'
 import { isEqual } from 'lodash'
 import { fetchCampaignById } from './backend'
-import { useError } from '../../../../../shared/src/util/useObservable'
+import { useObservable } from '../../../../../shared/src/util/useObservable'
 import * as H from 'history'
 import { CampaignBurndownChart } from './BurndownChart'
 import { Subject, of, merge } from 'rxjs'
-import { switchMap, distinctUntilChanged, repeatWhen, delay } from 'rxjs/operators'
+import { switchMap, distinctUntilChanged } from 'rxjs/operators'
 import { ThemeProps } from '../../../../../shared/src/theme'
 import { CampaignActionsBar } from './CampaignActionsBar'
 import { CampaignChangesets } from './changesets/CampaignChangesets'
-import { pluralize } from '../../../../../shared/src/util/strings'
 import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
 import { PlatformContextProps } from '../../../../../shared/src/platform/context'
 import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
@@ -45,47 +44,23 @@ export const CampaignDetails: React.FunctionComponent<Props> = ({
     telemetryService,
     _fetchCampaignById = fetchCampaignById,
 }) => {
-    // For errors during fetching
-    const triggerError = useError()
-
-    /** Retrigger campaign fetching */
+    /** Retrigger fetching */
     const campaignUpdates = useMemo(() => new Subject<void>(), [])
-    /** Retrigger changeset fetching */
-    const changesetUpdates = useMemo(() => new Subject<void>(), [])
-
-    const [campaign, setCampaign] = useState<CampaignFields | null>()
 
     useEffect(() => {
         telemetryService.logViewEvent(campaignID ? 'CampaignDetailsPage' : 'NewCampaignPage')
     }, [campaignID, telemetryService])
 
-    useEffect(() => {
-        if (!campaignID) {
-            return
-        }
-        // on the very first fetch, a reload of the changesets is not required
-        let isFirstCampaignFetch = true
-
-        // Fetch campaign if ID was given
-        const subscription = merge(of(undefined), campaignUpdates)
-            .pipe(
-                switchMap(() =>
-                    _fetchCampaignById(campaignID).pipe(repeatWhen(observer => observer.pipe(delay(5000))))
+    const campaign: CampaignFields | null | undefined = useObservable(
+        useMemo(
+            () =>
+                merge(of(undefined), campaignUpdates).pipe(
+                    switchMap(() => _fetchCampaignById(campaignID)),
+                    distinctUntilChanged((a, b) => isEqual(a, b))
                 ),
-                distinctUntilChanged((a, b) => isEqual(a, b))
-            )
-            .subscribe({
-                next: fetchedCampaign => {
-                    setCampaign(fetchedCampaign)
-                    if (!isFirstCampaignFetch) {
-                        changesetUpdates.next()
-                    }
-                    isFirstCampaignFetch = false
-                },
-                error: triggerError,
-            })
-        return () => subscription.unsubscribe()
-    }, [campaignID, triggerError, changesetUpdates, campaignUpdates, _fetchCampaignById])
+            [campaignID, campaignUpdates, _fetchCampaignById]
+        )
+    )
 
     // Is loading.
     if (campaign === undefined) {
@@ -100,8 +75,6 @@ export const CampaignDetails: React.FunctionComponent<Props> = ({
         return <HeroPage icon={AlertCircleIcon} title="Campaign not found" />
     }
 
-    const totalChangesetCount = campaign.changesets.totalCount
-
     return (
         <>
             <PageTitle title={campaign.name} />
@@ -112,30 +85,20 @@ export const CampaignDetails: React.FunctionComponent<Props> = ({
                 createdAt={campaign.createdAt}
                 description={campaign.description}
             />
-            {totalChangesetCount > 0 && (
-                <>
-                    <h3 className="mt-4 mb-2">Progress</h3>
-                    <CampaignBurndownChart
-                        changesetCountsOverTime={campaign.changesetCountsOverTime}
-                        history={history}
-                    />
-                    <h3 className="mt-4 d-flex align-items-end mb-0">
-                        {totalChangesetCount} {pluralize('Changeset', totalChangesetCount)}
-                    </h3>
-                    <CampaignChangesets
-                        campaignID={campaign.id}
-                        viewerCanAdminister={campaign.viewerCanAdminister}
-                        changesetUpdates={changesetUpdates}
-                        campaignUpdates={campaignUpdates}
-                        history={history}
-                        location={location}
-                        isLightTheme={isLightTheme}
-                        extensionsController={extensionsController}
-                        platformContext={platformContext}
-                        telemetryService={telemetryService}
-                    />
-                </>
-            )}
+            <h3 className="mt-4 mb-2">Progress</h3>
+            <CampaignBurndownChart changesetCountsOverTime={campaign.changesetCountsOverTime} history={history} />
+            <CampaignChangesets
+                campaignID={campaign.id}
+                viewerCanAdminister={campaign.viewerCanAdminister}
+                changesetUpdates={campaignUpdates}
+                campaignUpdates={campaignUpdates}
+                history={history}
+                location={location}
+                isLightTheme={isLightTheme}
+                extensionsController={extensionsController}
+                platformContext={platformContext}
+                telemetryService={telemetryService}
+            />
         </>
     )
 }

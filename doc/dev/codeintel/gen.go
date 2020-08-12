634d11594987a46bdcf7e5bf74251fc3a719d136
Eric Fritz <eric@eric-fritz.com>
2020-08-12 13:50:42 -0500

    Update first draft of resolver/api sequence diagrams.

--- doc/dev/codeintel/gen.go
+++ doc/dev/codeintel/gen.go
@@ -1,3 +1,5 @@
 package architecture
 
 //go:generate sh -c "../../../node_modules/.bin/mmdc -i definitions.mermaid -o definitions.svg"
+//go:generate sh -c "../../../node_modules/.bin/mmdc -i references.mermaid -o references.svg"
+//go:generate sh -c "../../../node_modules/.bin/mmdc -i hover.mermaid -o hover.svg"
